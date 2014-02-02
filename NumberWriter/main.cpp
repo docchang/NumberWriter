db162d4db82f9dfe12ff743c73e15d0c3c7d42e5
Dominic Chang <doc.dominic@gmail.com>
2014-02-01 21:33:27 -0800

    Initial Commit

---
+++ NumberWriter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NumberWriter
+//
+//  Created by Dominic Chang on 2/1/14.
+//  Copyright (c) 2014 Dominic Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
